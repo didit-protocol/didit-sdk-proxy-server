e64d52d225eca773ae63b37a62610934014d20fd
Alberto <rosas.alberto.upc@gmail.com>
2023-04-24 12:15:45 +0800

    update version

--- examples/node/api/wallet_authorization.js
+++ examples/node/api/wallet_authorization.js
@@ -4,7 +4,9 @@ const axios = require("axios");
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
-    const { scope, wallet_address, claims } = req.body;
+    const { wallet_address } = req.body;
+    const scope = process.env.scope
+    const claims = process.env.claims
 
     const auth = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString("base64");
 
